ab615aa3a6f58b9fb7d41b2fb27656279e403761
Navaneeth Bysani <61292292+Navaneeth-Bysani@users.noreply.github.com>
2021-08-22 16:46:57 +0530

    implemented backend till sending emails
    sending emails is done

--- models/registrationModel.js
+++ models/registrationModel.js
@@ -1,10 +1,28 @@
-//email
-//name
-//phone number
-//college details
+const mongoose = require('mongoose');
+const validator = require('validator');
 
+const registrationSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : [true, 'user should have a name']
+    },
+    email : {
+        type : String,
+        required : [true, 'user should have an email'],
+        // unique : true,
+        lowercase : true,
+        validate : [validator.isEmail, 'Please provide a valid email']
+    },
+    phoneNumber : {
+        type : String,
+        required : [true]
+    },
+    emailVerificationCode : String,
+    emailVerified : {
+        type : Boolean,
+        default : false
+    },
+})
 
-//workshop details
-
-//payment details
-    //payment id etc
+const Registration = mongoose.model('Registration', registrationSchema);
+module.exports = Registration;
--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 const app = require('./app');
+const mongoose = require('mongoose');
+
+const MONGO_URL = "mongodb+srv://LinX:Navaneeth@LiX@linx.xx7ku.mongodb.net/LinX?retryWrites=true&w=majority";
+
+mongoose.connect(MONGO_URL, {
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+    useCreateIndex : true,
+    useFindAndModify : false
+}).then(() => {
+    console.log('connected to database');
+}).catch((err) => {
+    console.log(err);
+})
+
 const PORT = 5000;
 const server = app.listen(PORT, () => {
     console.log(`Listening to PORT - ${PORT}`);
+})
+
+process.on('unhandledRejection', err => {
+    console.log('Unhandled Rejection!! Shutting Down the server....');
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    })
 })

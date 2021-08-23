77badd883d82fcde646c41873b91bdac0009e758
kartikeya-123 <56500864+kartikeya-123@users.noreply.github.com>
2021-08-23 18:47:04 +0530

    d

--- app.js
+++ app.js
@@ -9,10 +9,13 @@ const app = express();
 app.use(cors());
 
 app.use(express.json({ limit: "10kb" }));
+
+app.use(express.static(path.join(__dirname, "client/build")));
+
 app.use(`/api/register`, registerRouter);
 
 app.get("*", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "/client/public/index.html"));
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
 
 app.all("*", (req, res, next) => {

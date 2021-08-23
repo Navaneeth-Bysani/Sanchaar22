f06a7a0a84cad4aaef87c16f11aa2bc46e4710c2
Navaneeth Bysani <61292292+Navaneeth-Bysani@users.noreply.github.com>
2021-08-23 23:35:35 +0530

    changed payment callback code

---
+++ client/src/views/PaymentStatus.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+// const url = require("url");
+import { useParams } from "react-router-dom";
+import AlertDialog from "../components/elements/MaterialModal";
+// import sections
+import PaymentStatusHero from "../components/sections/PaymentStatusHero";
+
+import Button from "./../components/elements/Button";
+import {
+  Container,
+  makeStyles,
+  Card,
+  CardContent,
+  CardActions,
+  Box,
+  Divider,
+  Typography,
+} from "@material-ui/core";
+import componentStyles from "./../assets/theme/Register";
+
+// import FeaturesTiles from '../components/sections/FeaturesTiles';
+import FeaturesSplit from "../components/sections/FeaturesSplit";
+import Testimonial from "../components/sections/Testimonial";
+import Cta from "../components/sections/Cta";
+
+
+const PaymentStatus = () => {
+    const useStyles = makeStyles(componentStyles);
+    const classes = useStyles();
+    const [message, setMessage] = useState("Loading...");
+    useEffect(() => {
+        console.log("here");
+        let link = window.location.href;
+        const idx = link.indexOf('callback');
+        const str = link.substring(idx);
+        axios.get(`/api/register/${str}`).then(res=> {
+            console.log(res);
+            setMessage("Payment Successful, Please check your emails");
+        }).catch(err => {
+            console.log(err);
+            setMessage("There was an error while making payment!\nPlease contact our team, if money is deducted");
+        })
+    }, [])
+  return (
+    <Container alignItems="center" classes={{ root: classes.containerRoot }}>
+    <PaymentStatusHero className="illustration-section-01" />
+    <Card
+    style={{
+      backgroundColor: "black",
+      textAlign: "center",
+      width: "80%",
+      maxWidth: "40rem",
+      margin: "0px auto",
+      padding: "10px",
+    }}
+  >
+    <CardContent>
+      <Typography>
+        {message}
+      </Typography>
+    </CardContent>
+  </Card>
+  </Container>
+    
+);
+};
+
+export default PaymentStatus;
--- controllers/registerController.js
+++ controllers/registerController.js
@@ -78,7 +78,7 @@ exports.makePayment = catchAsync(async (req, res, next) => {
   data.purpose = "workshop";
   data.amount = 600;
   data.buyer_name = registration.name;
-  data.redirect_url = `http://localhost:5000/api/register/callback?user_id=${registration._id}`;
+  data.redirect_url = `http://localhost:3000/callback?user_id=${registration._id}`;
   data.email = registration.email;
   data.phone = registration.phoneNumber;
   data.send_email - false;
@@ -126,6 +126,9 @@ exports.confirmPayment = catchAsync(async (req, res, next) => {
       text,
     };
     await email(emailOptions);
-    return res.redirect("https://wiss-sanchaar-2022.herokuapp.com/");
+    res.status(200).json({
+        "status" : "success",
+        "message" : "payment is successful"
+    })
   }
 });

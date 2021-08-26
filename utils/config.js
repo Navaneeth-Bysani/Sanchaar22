0e5366d7f61d38f9c0f88799567d0446ba0ebe5d
kartikeya-123 <56500864+kartikeya-123@users.noreply.github.com>
2021-08-26 18:58:47 +0530

    Production deployement

--- utils/config.js
+++ utils/config.js
@@ -5,6 +5,8 @@ let MONGODB_URI = process.env.MONGODB_URI;
 let NODE_ENV = process.env.NODE_ENV;
 let API_KEY = process.env.API_KEY;
 let AUTH_KEY = process.env.AUTH_KEY;
+let EMAIL_USERNAME = process.env.EMAIL_USERNAME;
+let EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
 
 module.exports = {
   PORT,
@@ -12,4 +14,6 @@ module.exports = {
   MONGODB_URI,
   API_KEY,
   AUTH_KEY,
+  EMAIL_USERNAME,
+  EMAIL_PASSWORD,
 };
--- utils/email.js
+++ utils/email.js
@@ -1,5 +1,6 @@
 const nodemailer = require("nodemailer");
 const AppError = require("./appError");
+const config = require("./config");
 
 const sendEmail = async (options) => {
   //create transport function
@@ -7,13 +8,15 @@ const sendEmail = async (options) => {
   let transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: "codelions.org@gmail.com",
-      pass: "codelions@1234",
+      // user: "codelions.org@gmail.com",
+      // pass: "codelions@1234",
+      user: config.EMAIL_USERNAME,
+      pass: config.EMAIL_PASSWORD,
     },
   });
 
   const mailOptions = {
-    from: "Wissenaire Sanchaar <codelions.org@gmail.com>",
+    from: "Wissenaire Sanchaar <wissenaire.sanchaar@gmail.com>",
     to: options.email,
     subject: options.subject,
     text: options.text,

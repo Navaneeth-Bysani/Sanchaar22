c4dfd017aad7c90470cca372e76b37920a07bbf2
kartikeya-123 <56500864+kartikeya-123@users.noreply.github.com>
2021-09-12 11:47:27 +0530

    Deploy

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-// const app = require("./app");
-// const mongoose = require("mongoose");
-// const dotenv = require("dotenv");
+const app = require("./app");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-// const config = require("./utils/config");
-// dotenv.config();
+const config = require("./utils/config");
+dotenv.config();
 
-// // const MONGO_URL = "mongodb+srv://LinX:Navaneeth@LiX@linx.xx7ku.mongodb.net/LinX?retryWrites=true&w=majority";
-// // console.log(config.MONGODB_URI);
+// const MONGO_URL = "mongodb+srv://LinX:Navaneeth@LiX@linx.xx7ku.mongodb.net/LinX?retryWrites=true&w=majority";
+// console.log(config.MONGODB_URI);
 // mongoose
 //   .connect(config.MONGODB_URI, {
 //     useNewUrlParser: true,
@@ -21,15 +21,15 @@
 //     console.log(err);
 //   });
 
-// // const PORT = 5000;
-// const server = app.listen(config.PORT, () => {
-//   console.log(`Listening to PORT - ${config.PORT}`);
-// });
+// const PORT = 5000;
+const server = app.listen(config.PORT, () => {
+  console.log(`Listening to PORT - ${config.PORT}`);
+});
 
-// process.on("unhandledRejection", (err) => {
-//   console.log("Unhandled Rejection!! Shutting Down the server....");
-//   console.log(err.name, err.message);
-//   server.close(() => {
-//     process.exit(1);
-//   });
-// });
+process.on("unhandledRejection", (err) => {
+  console.log("Unhandled Rejection!! Shutting Down the server....");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
